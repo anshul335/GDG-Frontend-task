04264894844289782a56bbe8b0553e0897c876ca
Anshul Agarwal <anshulagarwal337@gmail.com>
2025-08-05 16:10:27 +0530

    Adding Comapny Animation

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
-import company from '../../src/assets/Company.png'
+import Google from '../../src/assets/CompaniesImage/Google.png'
+import Amazon from '../../src/assets/CompaniesImage/Amazon.png'
+import Facebook from '../../src/assets/CompaniesImage/Facebook.png'
+import Netflix from '../../src/assets/CompaniesImage/Netflix.png'
+import Grab from '../../src/assets/CompaniesImage/Grab.png'
+import AirBnb from '../../src/assets/CompaniesImage/AirBnb.png'
+
+
+
 
 const Company = () => {
+  const companies = [
+    { id: 1, name: 'Google', image: Google },
+    { id: 2, name: 'Netflix', image: Netflix },
+    { id: 3, name: 'AirBnb', image: AirBnb},
+    { id: 4, name: 'Amazon', image: Amazon },
+    { id: 5, name: 'Facebook', image: Facebook },
+    { id: 6, name: 'Grab', image: Grab }
+  ]
   return (
-    <div className="flex justify-center items-center py-8 overflow-x-hidden dark:bg-black">
-      <img src={company} alt="companies" className="w-[70%] object-contain" />
+    <div className="flex w-full justify-center items-center py-8 overflow-x-hidden dark:bg-black">
+      <div className=' whitespace-nowrap overflow-auto no-scrollbar '>
+        <div className='whitespace-nowrap inline-block  animate-move '
+        >
+          {companies.map((company) => (
+            <img src={company.image} alt={company.name} key={company.id} className ="md:w-30 w-20 md:h-20 h-15  mx-[3vw] object-contain inline-block " />
+          ))}
+        </div>
+        <div className='whitespace-nowrap  animate-move md:mx-0 inline-block'>
+          {companies.map((company) => (
+            <img src={company.image} alt={company.name} key={company.id} className ="md:w-30 md:h-20 w-20 h-15  mx-[3vw] object-contain inline-block " />
+          ))}
+        </div>
+        
+        
+      </div>
     </div>
   );
 };
