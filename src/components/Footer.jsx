feed9f33c3b47f26936a42bc0bf912ea348fb72b
Anshul Agarwal <anshulagarwal337@gmail.com>
2025-08-05 10:44:23 +0530

    Adding Responsiveness to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
             <div className="max-w-4xl mx-auto px-6 text-center">
                 {/* Logo and tagline */}
                 <div className=" flex flex-col justify-between sm:flex-row items-center gap-10 mb-8  px-[22vw]">
-                <div className='relative flex items-center justify-center'>
-                    <img src={Polygon} alt="Skilline logo" className=" h-10" />
-                    <p className=" absolute left-3 text-lg text-white font-bold">Skilline</p>
-                </div>
-                <div className='w-[1px] h-10 bg-[#626381]'></div>
-                <span className="text-white text-lg">Virtual Class<br/> for Zoom</span>
+                    <div className='relative flex items-center justify-center'>
+                        <img src={Polygon} alt="Skilline logo" className=" h-10" />
+                        <p className=" absolute left-3 text-lg text-white font-bold">Skilline</p>
+                    </div>
+                    <div className='w-[1px] h-10 bg-[#626381] hidden md:block'></div>
+                    <span className="text-white text-lg">Virtual Class<br/> for Zoom</span>
                 </div>
 
                 {/* Newsletter subscription */}
@@ -20,26 +20,26 @@ const Footer = () => {
                     Subscribe to get our Newsletter
                 </h2>
                 <form className="flex flex-col sm:flex-row items-center justify-center gap-2">
-                <input
-                    type="email"
-                    placeholder="Your Email"
-                    className="w-full sm:w-auto bg-transparent border border-gray-400 text-gray-100 placeholder-gray-400 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button
-                    type="submit"
-                    className="bg-gradient-to-r from-[#545AE7] to-[#393FCF] hover:bg-blue-700 text-white font-medium rounded-full px-6 py-2 transition"
-                >
-                    Subscribe
-                </button>
+                    <input
+                        type="email"
+                        placeholder="Your Email"
+                        className=" sm:w-auto bg-transparent border border-gray-400 text-gray-100 placeholder-gray-400 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button
+                        type="submit"
+                        className="bg-gradient-to-r from-[#545AE7] to-[#393FCF] hover:bg-blue-700 text-white font-medium rounded-full px-6 py-2 transition"
+                    >
+                        Subscribe
+                    </button>
                 </form>
 
                 {/* Footer links */}
                 <div className="mt-8 text-sm text-[#B2B3CF] dark:text-[#d6cfcf] space-x-2">
-                <a href="#careers" className="hover:text-gray-100">Careers</a>
-                <span>|</span>
-                <a href="#privacy" className="hover:text-gray-100">Privacy Policy</a>
-                <span>|</span>
-                <a href="#terms" className="hover:text-gray-100"> Terms & Conditions</a>
+                    <a href="#careers" className="hover:text-gray-100">Careers</a>
+                    <span>|</span>
+                    <a href="#privacy" className="hover:text-gray-100">Privacy Policy</a>
+                    <span>|</span>
+                    <a href="#terms" className="hover:text-gray-100"> Terms & Conditions</a>
                 </div>
 
                 {/* Copyright */}
