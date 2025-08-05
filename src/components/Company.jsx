5f7d14d3e9ca5245729200b8988922cd6abec5c4
Anshul Agarwal <anshulagarwal337@gmail.com>
2025-08-05 22:31:30 +0530

    Adding Animation in Companies Section

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -6,50 +6,44 @@ import Netflix from '../../src/assets/CompaniesImage/Netflix.png'
 import Grab from '../../src/assets/CompaniesImage/Grab.png'
 import AirBnb from '../../src/assets/CompaniesImage/AirBnb.png'
 
-
-
-
 const Company = () => {
   const companies = [
     { id: 1, name: 'Google', image: Google },
     { id: 2, name: 'Netflix', image: Netflix },
-    { id: 3, name: 'AirBnb', image: AirBnb},
+    { id: 3, name: 'AirBnb', image: AirBnb },
     { id: 4, name: 'Amazon', image: Amazon },
     { id: 5, name: 'Facebook', image: Facebook },
     { id: 6, name: 'Grab', image: Grab }
   ]
+
+  // Duplicate the array so the strip can loop seamlessly
+  const looped = [...companies, ...companies]
+
   return (
-    <div className="flex flex-col w-full justify-center items-center py-8 overflow-x-hidden dark:bg-black">
-      <h2 className='text-[#696984] dark:text-[#d6cfcf] text-lg'>Trusted by 5,000+ Companies Worldwide</h2>
-      <div className=' whitespace-nowrap overflow-auto no-scrollbar '>
-        <div className='whitespace-nowrap inline-block  animate-move  '
-        >
-          {companies.map((company) => (
-            <div className='whitespace-nowrap inline-block '>
-            <img src={company.image} alt={company.name} key={company.id} className ="md:w-30 w-20 md:h-20 h-15  mx-[3vw] object-contain inline-block " />
+    <div className="flex flex-col w-full items-center py-8 overflow-hidden dark:bg-black">
+      <h2 className="text-[#696984] dark:text-[#d6cfcf] text-lg">
+        Trusted by 5,000+ Companies Worldwide
+      </h2>
+
+      {/* 
+        outer div must hide overflow
+        inner div is super-wide flex with animation
+      */}
+      <div className="w-full overflow-hidden">
+        <div className="flex animate-scroll">
+          {looped.map((company, idx) => (
+            <div key={idx} className="flex-shrink-0 mx-[3vw]">
+              <img
+                src={company.image}
+                alt={company.name}
+                className="h-15 w-20 md:h-20 md:w-30 object-contain"
+              />
             </div>
           ))}
         </div>
-        <div className='whitespace-nowrap  animate-move md:mx-0 inline-block '>
-          {companies.map((company) => (
-            <div className='whitespace-nowrap inline-block'>
-              <img src={company.image} alt={company.name} key={company.id} className ="md:w-30 md:h-20 w-20 h-15  mx-[3vw] object-contain inline-block " />
-            </div>
-          ))}
-        </div>
-        <div className='whitespace-nowrap  animate-move md:mx-0 inline-block '>
-          {companies.map((company) => (
-            <div className='whitespace-nowrap inline-block'>
-              <img src={company.image} alt={company.name} key={company.id} className ="md:w-30 md:h-20 w-20 h-15  mx-[3vw] object-contain inline-block " />
-            </div>
-          ))}
-        </div>
-        
-        
       </div>
     </div>
-  );
-};
-
+  )
+}
 
 export default Company
